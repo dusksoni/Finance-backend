19798508da444548c74f0f793ad7bd32e92d3aff
Sagar soni <Dusksoni@gmail.com>
2025-05-04 23:10:38 +0530

    deploy

--- server.js
+++ server.js
@@ -36,4 +36,4 @@ app.use("/api/region", regionRoute);
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servers running on http://localhost:${PORT}`));
